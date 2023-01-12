10ada419b59961a3ce2505d9e6e60735da23d6b8
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2023-01-13 01:24:21 +1100

    crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import PostsProvider from './Components/PostsProvider';
+import HomePage from './Pages/HomePage';
+import CreatePost from './Pages/CreatePost';
+import ViewPost from './Pages/ViewPost';
+import EditPost from './Pages/EditPost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostsProvider>
+        <Router>
+          <Routes>
+            <Route path='/' exact element={<HomePage />} ></Route>
+            <Route path='/posts/new' element={<CreatePost />} ></Route>
+            <Route path='/posts/edit/:id' element={<EditPost />} ></Route>
+            <Route path='/posts/:id' element={<ViewPost />} ></Route>
+          </Routes>
+        </Router>
+      </PostsProvider>
     </div>
   );
 }
